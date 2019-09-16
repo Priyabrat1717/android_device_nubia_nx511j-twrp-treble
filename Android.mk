2e10bbc762041fc524191ccd0e3e834ef8f27bea
PS1717 <38763217+Priyabrat1717@users.noreply.github.com>
2019-09-16 12:12:16 +0530

    mirage01a:Initialize treble

--- Android.mk
+++ Android.mk
@@ -23,7 +23,7 @@
 # *not* include it on all devices, so it is safe even with hardware-specific
 # components.
 
-ifneq ($(filter nx511j,$(TARGET_DEVICE)),)
+ifneq ($(filter mirage01a,$(TARGET_DEVICE)),)
 
 LOCAL_PATH := $(call my-dir)
 
